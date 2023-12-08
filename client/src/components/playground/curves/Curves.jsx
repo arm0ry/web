dbdcefa6aa8f58bab41e199719dffd801cdbd856
charlie <charlie@mobius.local>
2023-12-08 21:14:08 +0800

    ✨

--- client/src/components/playground/curves/Curves.jsx
+++ client/src/components/playground/curves/Curves.jsx
@@ -2,85 +2,94 @@ import React, { useState, useEffect, useMemo } from "react";
 import { useGlobalContext } from "@context/store";
 import { KaliCurve } from "../../../contract";
 import { ethers } from "ethers";
-import { useContractInfiniteReads, useContractRead, paginatedIndexesConfig } from "wagmi";
+import {
+  useContractInfiniteReads,
+  useContractRead,
+  paginatedIndexesConfig,
+} from "wagmi";
 import CurveCard from "./CurveCard";
 
 const Curves = () => {
   const [curves, setCurves] = useState();
   const { data, fetchNextPage, isFetched } = useContractInfiniteReads({
-    cacheKey: 'curves',
+    cacheKey: "curves",
     ...paginatedIndexesConfig(
       (index) => {
         return [
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getCurveOwner',
+            functionName: "getCurveOwner",
             args: [index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getPrice',
+            functionName: "getPrice",
             args: [true, index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getPrice',
+            functionName: "getPrice",
             args: [false, index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getCurveTreasury',
+            functionName: "getCurveTreasury",
             args: [index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getCurveSupply',
+            functionName: "getCurveSupply",
             args: [index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getCurveData',
+            functionName: "getCurveData",
             args: [index],
           },
           {
             address: KaliCurve.address,
             abi: KaliCurve.abi,
-            functionName: 'getImpactDao',
+            functionName: "getImpactDao",
             args: [index],
           },
-        ]
+        ];
       },
-      { start: 1, perPage: 5, direction: 'increment' },)
-  })
+      { start: 1, perPage: 5, direction: "increment" }
+    ),
+  });
 
   useEffect(() => {
-    const tempData = data.pages[0]
-    let innerArry = []
-    let outerArry = [];
-    for (let i = 0; i < tempData.length; i++) {
-      innerArry.push(tempData[i])
-      if (i % 7 === 6) {
-        outerArry.push(innerArry)
-        innerArry = []
+    if (data) {
+      const tempData = data.pages[0];
+      let innerArry = [];
+      let outerArry = [];
+      for (let i = 0; i < tempData.length; i++) {
+        innerArry.push(tempData[i]);
+        if (i % 7 === 6) {
+          outerArry.push(innerArry);
+          innerArry = [];
+        }
       }
+      setCurves(outerArry);
     }
-    setCurves(outerArry)
-
-  }, [data])
+  }, [data]);
 
   return (
     <>
       <div className="grid grid-cols-1 gap-10 p-4">
-
-        {curves !== undefined ? curves?.map((curve, id) => {
-          return <CurveCard key={id} curve={curve} />;
-        }) : <></>}
+        {curves !== undefined ? (
+          curves?.map((curve, id) => {
+            return <CurveCard key={id} curve={curve} />;
+          })
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
