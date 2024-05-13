599a59656bb1f4b82af5262d1f9e600245150e18
audsssy <stamfordh@hotmail.com>
2024-05-13 11:49:32 -0700

    Update CommonsReports.jsx

--- client/src/components/playground/reports/communities/CommonsReports.jsx
+++ client/src/components/playground/reports/communities/CommonsReports.jsx
@@ -59,7 +59,7 @@ const CommonsReports = () => {
     <>
       <div className="flex flex-col">
         <label className="p-4 mb-2 block text-2xl font-bold text-gray-900 mx-auto">
-          Pooled Impact
+          Pooled Impact [WIP]
         </label>
         <div className="w-5/6 mx-auto mb-10 flex flex-row rounded-lg px-5 py-5 bg-slate-50 space-x-5">
           <label className="py-5 text-md font-normal text-gray-900">
@@ -74,13 +74,14 @@ const CommonsReports = () => {
             <div className="space-y-1">
               <label className=" block text-md font-normal text-gray-700">
                 We bond each SVG NFT to its own <a target="_blank" href="https://medium.com/@simondlr/tokens-2-0-curved-token-bonding-in-curation-markets-1764a2e0bee5" className="underline"
-                >bonding curve</a> to automate trading of the NFTs and distribution of transaction fees.
+                >bonding curve</a> to automate distribution of local currencies and stablecoins. Credit: <a target="_blank" href="https://p2pfoundation.net/" className="underline"
+                >P2P Foundation</a> & <a target="_blank" href="http://regenerosity.com/" className="underline">Regeneorsity</a>.
               </label>
             </div>
             <div className="space-y-1">
 
               <label className=" block text-md font-normal text-gray-700">
-                Bonding curves are autonomous markets, and they ensure token liquidity, facilitate price discovery, and increase asset efficiency.
+                Bonding curves are autonomous markets. Its programmability enables communities to transact for local products and serivces, and also segment portions of transaction for infinite potential. They ensure token liquidity, facilitate price discovery, and increase asset efficiency.
               </label>
             </div>
             <div className="space-y-1">
@@ -93,8 +94,9 @@ const CommonsReports = () => {
         <div className="flex flex-col space-y-10">
           <div className="" >
             <SupportCard
-              title={"Flat Donation: Community Onboarding"}
-              engDescription={"Communities may use SVG NFTs to showcase live participation in community activities. This particular NFT reflects participation in the 'Create a List' tutorial. We welcome any flat donations for our effort."}
+              title={"Flat Collection: Community Onboarding"}
+              description={"Flat Collections are static in amount to collect, the amount does not chage per token supplies or time."}
+              engDescription={"Flat Collections are appropriate for Lists that involve personal time worked."}
               curveId={5}
               svg={svg}
               supply={listTokenSupply}
@@ -102,7 +104,8 @@ const CommonsReports = () => {
           </div >
           <div>
             <SupportCard
-              title={"Curved Support: Wildnerness Park"}
+              title={"Curved Support: Wildnerness Park (IRL activities)"}
+              description={"Parallel Ownership"}
               engDescription={"While the trail posts are listed in the NFT rather than displayed beautifully over a satelite map of the trail, we can get an idea of how wilderness parks, managed by local governments or communities, may use this as real-time heatmap for trails. Most trail posts today have QR codes on them already, we can repurpose them for hikers to submit onchain check-ins and report trail conditions. Kind of like Waze for trails! In addition to being the medium for fundraising, this NFT can also help locals identify traffic on trails, assess maintainenance schedule, and potentially help locate hikers when they get lost. Best of all, this is cheap to do!"}
               curveId={6}
               svg={wpSvg}
@@ -111,8 +114,8 @@ const CommonsReports = () => {
           </div>
           <div>
             <SupportCard
-              title={"Curved Support 2: Music Collection"}
-              engDescription={"The same NFT may also be used as a living album cover."}
+              title={"Curved Support 2: Music Collection (IP)"}
+              engDescription={"Practically, Curved Support adds a layer on top of Flat Collection & an offers opportunities for supporters to exit. It is appropriate for Lists that involve personal time worked, produce local values that circulate within local community, and external values that may enter or leave the local economy. The flat base of Curved Support operate similar to that of Flat Collections, and the upward curve represent values sustained by the collective impact and influence of the Lists. Here, we use the same NFT as a living album cover that showcase interactions with its content in real-time."}
               curveId={9}
               svg={nuSvg}
               supply={nuSupply}
@@ -121,7 +124,7 @@ const CommonsReports = () => {
           <div>
             <SupportCard
               title={"Harberger Sponsor: g0v Hackath0n [WIP]"}
-              engDescription={"Similar to how TESTNET Wilderness Park may showcase a heat map of trail activities using the NFT, hackathon hosts may use similar techniques to preserve cumulative participation. What is different here is the use of Harberger Tax as the NFT's pricing and ownership (serial) mechanism, which is appropriate for supporters looking for more exclusive ownership and relationship with the hackathon hosts. "}
+              engDescription={"In addition to using bonding curves as the pricing and ownership mechanism for Lists, we can also use Harberger Tax to maintain serial ownership of the Lists. This mechanism is appropriate for supporters looking for more exclusive ownership and relationship with the Lists owners. "}
               curveId={9}
               svg={mSvg}
               supply={hackathonTokenSupply}
