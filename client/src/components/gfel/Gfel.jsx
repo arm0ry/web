43c59e71c978c610971bea3ff9d14dc25c7a5d26
audsssy <stamfordh@hotmail.com>
2024-09-11 21:10:30 -0700

    Update Gfel.jsx

--- client/src/components/gfel/Gfel.jsx
+++ client/src/components/gfel/Gfel.jsx
@@ -6,9 +6,6 @@ import { DynamicWidget } from "@dynamic-labs/sdk-react";
 import useWriteContract from "@hooks/useWriteContract";
 import { useAccount, useContractRead } from "wagmi";
 import { pushAlert } from "@context/actions/alertAction";
-import { shortenAddress } from "@utils/shortenAddress";
-import { Avatar } from "@components";
-
 
 import BULLETIN_ABI from "../../contract/playground/Bulletin.json";
 import LOGGER_ABI from "../../contract/playground/Log.json";
@@ -17,28 +14,49 @@ import TOKEN_CURVE_ABI from "../../contract/playground/TokenCurve.json";
 import ResponseCard from '../playground/responses/ResponseCard';
 
 export const Bulletin = {
-  address: "0x84c56F92DB38A08304965C888C530c805A1BC523",
+  address: "0x57443571dc66B666403Bf3eb581691916d5656c7",
   abi: BULLETIN_ABI,
 };
 
 export const Logger = {
-  address: "0x6F23F3CB957Bee8c3CDCe348902d34b629f65946",
+  address: "0xCDBaf7bA0aa88c6DA10573e98b0b94B0a6EcD03C",
   abi: LOGGER_ABI,
 };
 
 export const TokenMinter = {
-  address: "0x98C826A8e48A936278Dc3f2D31c4E4d3E18341Cc",
+  address: "0x7B696A60A2658577369C6ad0CB0aCdcd728D502E",
   abi: TOKEN_MINTER_ABI,
 };
 
 export const TokenCurve = {
-  address: "0x925a2f9791c6A40E76105F14544500F6FDbCef83",
+  address: "0x581135BCd0fCE63ad856676DA1A1a434f5f606F7",
   abi: TOKEN_CURVE_ABI,
 };
+
+const MoodRadio = ({ moon, value, register }) => {
+    return (
+      <>
+        <div className="flex items-center m-2">
+          <input
+            type="radio"
+            value={value}
+            {...register("moon")}
+          />
+
+          <label
+            className="ml-2 text-sm font-normal text-gray-900 "
+          >
+            {moon}
+          </label>
+        </div>
+      </>
+    );
+  };
 
 const Gfel = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const [inPrepare, setInPrepare] = useState(false);
+  const [error, setError] = useState('');
   const { address, isConnected } = useAccount();
   const [touchpoints, setTouchpoint] = useState([]);
 
@@ -46,7 +64,7 @@ const Gfel = () => {
     register,
     handleSubmit
   } = useForm({
-    defaultValues: { question1: "", question2: "", feedback: ""},
+    defaultValues: { moon: "2", question: ""},
   }); 
 
   const { write: log, state: logState } = useWriteContract({
@@ -71,7 +89,7 @@ const Gfel = () => {
     args: [1]
   })
   
-
+console.log(svg)
 
 
   const clickMint = async () => {
@@ -113,9 +131,20 @@ const Gfel = () => {
     let structuredData = ethers.constants.HashZero;
     const abiCoder = ethers.utils.defaultAbiCoder;
 
+
+    if (data.moon == "2") {
+      setError("Must fill all fields.")
+      return;
+    } else {
+      setError("")
+    }
     console.log(data, structuredData)
 
-    structuredData = abiCoder.encode(["uint256", "uint256"], [data.question1, data.question2]);
+    try {
+      structuredData = abiCoder.encode(["uint256", "uint256"], [data.moon, data.question]);
+    } catch (err) {
+      console.log(err)
+    }
 
 
 
@@ -136,9 +165,6 @@ const Gfel = () => {
   useEffect(() => {
     const loadTps = async () => {
       if (parseInt(nonce._hex) > 0 && parseInt(logId._hex) > 0) {
-        console.log("hello", )
-
-        
         const logger = new ethers.Contract(Logger.address, Logger.abi, provider);
         let _tps = [];
         
@@ -146,7 +172,7 @@ const Gfel = () => {
           try {
             const log = await logger.getLog(i);
             const tps = await logger.getTouchpointsByLog(i);
-            console.log(tps)
+            // console.log(tps)
   
             for (let j = 0; j <= tps.length; j++) {
               console.log(tps[j][3])
@@ -202,7 +228,7 @@ const Gfel = () => {
               {/* NFT */}
               <div className='flex w-1/2 h-full justify-center items-center bg-slate-300'>
                 <img
-                className="ring-1 ring-slate-400 opacity-100 blur-0 z-[10] m-1 rounded-lg w-4/5"
+                className="ring-1 ring-slate-400 opacity-100 blur-0 z-[10] m-1 rounded-lg w-4/5 2xl:w-3/5"
                 src={`data:image/svg+xml;utf8,${encodeURIComponent(svg)}`}
                 alt="Supporter Token"
                   ></img>
@@ -213,58 +239,58 @@ const Gfel = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
 
                 {/* Inputs */}
-                <div className='flex flex-col w-full space-y-4 mb-6 items-start my-10'>
-                  <div className="flex flex-col space-y-2">
-                    <label className=" block text-sm font-medium text-gray-900 lg:text-md md:text-sm sm:text-left">
-                      Did you have coffee with food?
-                    </label>
+                  <div className='flex flex-col w-full space-y-3 mb-4 items-start my-10'>
+                    <div className="flex flex-col space-y-1">
+                      <label className=" block text-sm font-medium text-gray-900 lg:text-md md:text-sm sm:text-left">
+                        Did you have coffee with food?
+                      </label>
                     
-                    <input required type="number" max={1} min={0}
-                      {...register("question1")}
-                      className="rounded-md text-center p-1 w-full" placeholder="1" />
-                  </div>
+                      <MoodRadio moon="Yes" value={1} register={register} />
+                      <MoodRadio moon="No" value={0} register={register} />
+                    </div>
 
-                  <div className="flex flex-col space-y-2">
-                    <label className=" block text-sm font-medium text-gray-900 ">
-                      How are you feeling?
-                    </label>
-                    
-                    <input required type="number" max={10} min={1}
-                      {...register("question2")}
-                      className="rounded-md text-center p-1 w-full" placeholder="1" />
-                  </div>
+                    <div className="flex flex-col space-y-1">
+                      <label className=" block text-sm font-medium text-gray-900 ">
+                        How are you feeling?
+                      </label>
+                      
+                      <input required type="number" max={10} min={1}
+                        {...register("question")}
+                        className="rounded-md text-center p-1 w-full" placeholder="1" />
+                    </div>
 
-                  <div className="flex flex-col space-y-2 w-full">
-                    <label className=" block text-sm font-medium text-gray-900 ">
-                      Feedback
-                    </label>
-                    
-                    <textarea
-                      id="feedback"
-                      className="w-full h-100vh rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-                      placeholder="Fried chicken was so good~"
-                      {...register("feedback")}
-                    ></textarea>
-                    
+                    <div className="flex flex-col space-y-2 w-full">
+                      <label className=" block text-sm font-medium text-gray-900 ">
+                        Feedback
+                      </label>
+                      
+                      <textarea
+                        id="feedback"
+                        className="w-full h-100vh rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                        placeholder="Fried chicken was so good~"
+                        {...register("feedback")}
+                      ></textarea>
                     </div>
                 
                 </div>  
 
-                {/* Buttons */}
+                  {/* Buttons */}
+                  <div className='flex w-full text-sm text-yellow-800 items-center justify-center pb-2'>{(error != "") ? error : "" }</div>
                   <div className='flex w-full items-center justify-center pb-2'>
                      <button
                         type="submit"
                         disabled={logState.writeStatus > 0}
-                        className="text-gray px-auto flex w-4/5 h-2/3 bg-yellow-300 rounded-md flex-row items-center justify-center py-2 text-center text-base font-medium transition duration-300 ease-in-out  hover:ring-4 hover:ring-yellow-200 active:ring-2 disabled:pointer-events-none disabled:opacity-25"
+                        className="text-gray px-auto flex w-4/5 h-2/3 bg-yellow-300 rounded-md flex-row items-center justify-center py-2 text-center text-sm transition duration-300 ease-in-out  hover:ring-4 hover:ring-yellow-200 active:ring-2 disabled:pointer-events-none disabled:opacity-25"
                     >     
                       {(logState.writeStatus === 0) && (inPrepare ? "Wait..." : "Share")}
                       {(logState.writeStatus > 0) && <Spinner />}
                       <div className={`${(logState.writeStatus > 0) ? "ml-2" : ""}`}>
-                        {(logState.writeStatus === 1) && "Waiting for approval"}
+                        {(logState.writeStatus === 1) && "pending"}
                         {(logState.writeStatus === 2) && "pending"}
                       </div>
                     </button>
                   </div>
+                    
                 </form>   
               </div>
             </div>
@@ -274,10 +300,6 @@ const Gfel = () => {
                 <div className='flex w-full h-full font-medium ml-4 mt-2'>Feedback</div>
               
               <div className="m-4 grid grid-cols-2 gap-5 xl:grid-cols-3 2xl:grid-cols-4">
-                {/* <div className="bg-slate-100 text-slate-400 h-32 rounded-lg flex items-center justify-center">
-                      Patiently waiting... | 等待中... 
-                </div> */}
-                
                 {(touchpoints.length > 0) ? (touchpoints.reverse().map((response, id) => {
                   return <ResponseCard key={id} response={response} />
                 }))
